b5b53b6c704c0ef7c5d207534d7314037e9e0197
alvimlops <alvim.lopez@gmail.com>
2021-10-19 21:07:30 -0300

    criando projeto para cadratro de funcionario

---
+++ src/aplication/Program.java
@@ -0,0 +1,44 @@
+package aplication;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Employee;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		List<Employee> list = new ArrayList<>();
+		
+		System.out.print("Quantos funcionarios  são registrados");
+		int N = sc.nextInt();
+		 for (int i=0; i<N; i++) {
+			 System.out.println("Funcionario #" + (i + 1) + ":");
+			 System.out.print("ID: ");
+			 Integer id = sc.nextInt();
+			 System.out.print("Name: ");
+			 sc.nextLine();
+			 String name = sc.nextLine();
+			 System.out.print("Salary: ");
+			 Double salary = sc.nextDouble();
+			 
+			 Employee emp = new Employee (id, name, salary);
+			 list.add(emp);
+		 }
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+	}
+
+}
