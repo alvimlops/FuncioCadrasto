fc5e6c7b8b92ee8c523302057e00c2fcd9895b75
alvimlops <alvim.lopez@gmail.com>
2021-10-27 11:42:12 -0300

    atualizando a lista de funcionarios

--- src/entities/Employee.java
+++ src/entities/Employee.java
@@ -52,7 +52,9 @@ public class Employee {
 		salary = salary + salary * percentage / 100.0;
 	}
 	
-	
+	public String toString() {
+		return id + ", " + name + ", " + String.format("%.2f", salary);
+	}
 	
 	
 }
